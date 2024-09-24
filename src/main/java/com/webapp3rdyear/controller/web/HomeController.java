c24ace64d465571e80a58fa30a087a3ea4e28044
RioHoang <131158772+Rio219@users.noreply.github.com>
2024-09-24 11:25:16 +0700

    done setup for user

---
+++ src/main/java/com/webapp3rdyear/controller/web/HomeController.java
@@ -0,0 +1,18 @@
+package com.webapp3rdyear.controller.web;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/home"})
+public class HomeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/views/web/home.jsp").forward(request, response);
+	}
+}
